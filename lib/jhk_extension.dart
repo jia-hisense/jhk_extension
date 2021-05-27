33974fa878cfcd9c8dfdc69a459600e7855b221f
jiafujia <jiafujia@hisense.com>
2021-05-27 12:16:43 +0800

    新增AddTwo方法

--- lib/jhk_extension.dart
+++ lib/jhk_extension.dart
@@ -4,4 +4,6 @@ library jhk_extension;
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  /// Returns [value] plus 2.
+  int addTwo(int value) => value + 2;
 }
